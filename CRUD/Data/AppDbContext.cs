3f3800fdc2cfd6887c80ed550f56e0f6163fa417
Benbormey <154697601+BenBormey@users.noreply.github.com>
2024-10-08 10:06:13 +0700

    Add project files.

---
+++ CRUD/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
